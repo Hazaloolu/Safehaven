185e9b4cc4b37d747f7e44e82c35779bb8574f47
Adetunji Adetayo <104958004+Navlos@users.noreply.github.com>
2023-05-25 18:17:56 -1200

    Update settings.py

--- safehavens/settings.py
+++ safehavens/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
 
-ALLOWED_HOSTS = ['safehaven-production.up.railway.app','127.0.0.1']
+ALLOWED_HOSTS = ['campusHome.up.railway.app','127.0.0.1']
 
 
 # Application definition

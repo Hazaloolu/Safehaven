27e45dc462d420b368d3d6cd17061f2766849272
Adetunji Adetayo <104958004+Navlos@users.noreply.github.com>
2023-05-26 20:52:25 -1200

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/',views.user_login,name='login'),
     path('logout/',views.logout_view,name='logout_view'),
+path('csrf-failure/', views.csrf_failure_view, name='csrf_failure')
  
   
    
